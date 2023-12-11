3abbacc69b4441879ffd73bd2bde64426ec48e41
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-12-10 23:13:37 -0300

    feat: add API getNews in home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,21 @@
+import { INewsMarvel } from 'src/types/INews';
+import { MarvelService } from '../../services/marvel.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  marvelData: INewsMarvel[] = [];
 
-  constructor() { }
+  constructor(private marvelService: MarvelService) {}
 
   ngOnInit(): void {
+    this.marvelService.getNews().subscribe((data) => {
+      this.marvelData = data.data.results;
+      console.log(this.marvelData);
+    });
   }
-
 }
